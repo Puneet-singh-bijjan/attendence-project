7ae8e4ebc99cfa505c4088575ef51a3b0175d862
Puneet Singh <puneetsingh8120@gmail.com>
2022-03-01 09:50:10 +0530

    working on frontend part

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const res = require('express/lib/response');
+
+const app = express();
+
+// Activate EJS view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
+app.use(express.static('public')); // Serve static files (e.g. CSS files)
+
+app.get('/', function(){
+    res.render('index.ejs');
+});

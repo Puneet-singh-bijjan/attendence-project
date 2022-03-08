5106072881720ec706fd46607812b4d28b0387f8
Puneet Singh <puneetsingh8120@gmail.com>
2022-03-08 21:16:10 +0530

    files updated

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
-const res = require('express/lib/response');
+const path = require('path');
+const qrcode = require('qrcode');
+let QR;
+
+const generateQR = async text => {
+    try{
+        return qrcode.toDataURL(text);
+    } catch(err){
+        console.log(err);
+    }
+}
 
 const app = express();
 
@@ -10,6 +20,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
 app.use(express.static('public')); // Serve static files (e.g. CSS files)
 
-app.get('/', function(){
+app.get('/', function(req, res){
     res.render('index.ejs');
 });
+
+app.get('/qr', async function(req, res){
+    const QR = await generateQR('https://google.com');
+    res.render('aside-QR',{image:QR});
+});
+
+app.listen(3000);
+
+//data updated
